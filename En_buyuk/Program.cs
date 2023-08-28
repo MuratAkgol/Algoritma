cdfc685640cf87ce5bf267fb9679ff2e36e000d3
MuratAkgol <akgolmurat4@gmail.com>
2023-08-28 15:03:43 +0300

    Sor Çözümleri

---
+++ En_buyuk/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace En_buyuk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odada kaç kişi var?");
+            int Mamber = Convert.ToInt32(Console.ReadLine());
+            Count(Mamber);            
+        }
+        public static void Count(int a)
+        {
+            decimal c = 1;
+            decimal d = 1;
+            decimal e = a - 2;
+            for (int i = 1; i <= a; i++)
+            {
+                c = c * i;
+               
+                if (i == a)
+                {
+                    //Console.WriteLine(c);
+                }
+            }
+
+            for (int i = 1; i <= e; i++)
+            {
+                d = d * i;
+                if (i == e)
+                {
+                    Console.WriteLine(c/(2*d) + " defa el sıkışılır.");
+                    Console.ReadLine() ;
+                }
+            }
+        }
+    }
+}
---
+++ faktoriyel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace faktoriyel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı Giriniz");
+            int a = Convert.ToInt32(Console.ReadLine());
+            decimal c = 1;
+            for (int i = 1; i < a+1; i++)
+            {
+              
+                //int b = 1;
+                c = c * i;
+               
+                if (i == a)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+            
+        Console.ReadLine();
+        }
+    }
+}
