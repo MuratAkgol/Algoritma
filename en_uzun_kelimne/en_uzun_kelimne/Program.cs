3d54c38eafd3e14261dbe66c2b0bac6d6d1b338a
MuratAkgol <akgolmurat4@gmail.com>
2023-09-15 17:01:24 +0300

    En uzun Kelime

---
+++ en_uzun_kelimne/en_uzun_kelimne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace en_uzun_kelimne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bİr cümle giriniz");
+            string cümle = Console.ReadLine();
+
+            Console.WriteLine("Bu metindeki en uzun kelime: "+cls_uzun.uzun_olan(cümle));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ en_uzun_kelimne/en_uzun_kelimne/cls_uzun.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace en_uzun_kelimne
+{
+    internal class cls_uzun
+    {
+        public static string uzun_olan(string kelime)
+        {
+            string[] ayir = kelime.Split(' ');
+            string enUzunKelime = ""; 
+
+            foreach (string kelimeParcasi in ayir)
+            {
+                if (kelimeParcasi.Length > enUzunKelime.Length)
+                {
+                    enUzunKelime = kelimeParcasi;
+                }
+            }
+
+            return enUzunKelime;
+        }
+
+    }
+}
